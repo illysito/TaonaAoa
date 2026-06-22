e7fda609885a6df45664f068385c767c819fce24
illysito <illyalukyanovdesign@gmail.com>
2026-06-22 18:26:01 +0100

    Update world.js

--- src/features/world/world.js
+++ src/features/world/world.js
@@ -70,7 +70,7 @@ async function worldHome() {
 
   // -------------------------------------------------------------- Background Plane --------------------------------------------------------------
 
-  const segments = isMobile() ? 80 : 160
+  const segments = isMobile() ? 80 : 140
   const planeGeometry = new THREE.PlaneGeometry(
     window.innerWidth,
     window.innerHeight,
